e0cc15f2e1d34a54cc21aca7ac17ce52eaf2b69a
nwaxon2020 <princenwachukwu308@yahoo.com>
2025-12-19 12:42:27 +0200

    nav updated

--- ui/nav.tsx
+++ ui/nav.tsx
@@ -4,8 +4,9 @@ export default function Nav(){
     return(
         <div className="flex justify-between items-center p-4 sm:px-6 bg-gray-900">
             {/* Nav Link */}
-            <Link href={"/"} className="p-1 w-30 flex gap-2 items-center justify-left bg-white rounded-sm">
-                <h2 className="text-blue-600 font-bold">Nomo Cars</h2>
+            <Link href={"/"} className="p-1 flex gap-2 items-center justify-left bg-white rounded-sm">
+                <h2 className="text-blue-600 font-bold inline">Nomo Cars</h2>
+                <img src="/favicon.png" alt="favicon.png" className="w-5 h-4" />
                 <i className="fa fa-car text-blue-600"></i>
             </Link>
 
