6b0ec5be46e5498e39779a5f18f497955ae87703
nwaxon2020 <princenwachukwu308@yahoo.com>
2025-12-19 12:59:25 +0200

    design logo name

--- ui/nav.tsx
+++ ui/nav.tsx
@@ -5,7 +5,12 @@ export default function Nav(){
         <div className="flex justify-between items-center p-4 sm:px-6 bg-gray-900">
             {/* Nav Link */}
             <Link href={"/"} className="p-1 flex gap-2 items-center justify-left bg-white rounded-sm">
-                <h2 className="text-blue-600 font-bold inline">Nomo Cars</h2>
+                <h2 className="md:text-xl font-extrabold italic text-blue-700 drop-shadow-md">
+                    Nomo <span className="text-yellow-500">Cars</span>
+                </h2>
+
+
+
                 <img src="/favicon.png" alt="favicon.png" className="w-5 h-4" />
                 <i className="fa fa-car text-blue-600"></i>
             </Link>
