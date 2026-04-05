d35fbad99ec70ea130799ce1ff22e19634058e82
Prince Ogbonnaya Nwachukwu <131527050+nwaxon2020@users.noreply.github.com>
2026-04-05 10:43:17 +0100

    Update HeroSection.tsx

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -79,10 +79,10 @@ export default function HeroSection({
             )}
 
             {!user && (
-              <div className="w-full col-span-3 mt-2 flex gap-3 max-w-lg">
+              <div className="w-full col-span-3 mt-2 flex flex-col md:flex-row gap-3 max-w-lg">
                 <Link
                   href="/signup"
-                  className="flex-1 w-full border-2 border-yellow-400/90 text-yellow-400 px-8 py-3 rounded-xl font-bold text-lg hover:bg-yellow-300/30 transition-all duration-300 flex items-center justify-center gap-3"
+                  className="md:flex-1 w-full border-2 border-yellow-400/90 text-yellow-400 px-8 py-3 rounded-xl font-bold text-lg hover:bg-yellow-300/30 transition-all duration-300 flex items-center justify-center gap-3"
                 >
                   <FaArrowLeft />
                   Sign Up
@@ -90,7 +90,7 @@ export default function HeroSection({
                 
                 <Link
                   href="/login" 
-                  className="flex-2 w-full bg-white text-blue-800 px-8 py-4 rounded-xl font-bold text-lg hover:bg-gray-100 transition-all duration-300 flex items-center justify-center gap-3 shadow-lg"
+                  className="md:flex-2 w-full bg-white text-blue-800 px-8 py-4 rounded-xl font-bold text-lg hover:bg-gray-100 transition-all duration-300 flex items-center justify-center gap-3 shadow-lg"
                 >
                   Login
                   <FaArrowRight />
